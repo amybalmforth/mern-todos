19bb2861f956cbb6e602f7616b6b13fdd985796f
Amy Balmforth <amy.balmforth@gmail.com>
2021-01-23 16:23:41 +0000

    todo controller

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const db = require('./db')
+const todoRouter = require('./routes/todo-router')
 
 const app = express()
 const apiPort = 3000
@@ -12,5 +13,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.use('/', todoRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
